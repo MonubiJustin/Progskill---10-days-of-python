2fb0d73ce844431981a2831d0bacf733253c894a
MonubiJustin <monubijustin@gmail.com>
2024-11-07 09:20:09 +0300

    Completed Day 3: Set up  Django admin and created sample posts

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import BlogPost
 
 # Register your models here.
-admin.site.register(BlogPost)
+@admin.register(BlogPost)
+class BlogPostAdmin (admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    list_filter = ('author', 'created_at')
