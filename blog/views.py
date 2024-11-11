e35a8e54c772e2a2ed94baf89f418d3c96e6328e
MonubiJustin <monubijustin@gmail.com>
2024-11-11 11:16:53 +0300

    Complete Day 5 Challenge: Add detailed post view with clickable titles on homepage

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at') # Ordering posts by creation date, newest first
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+# Retrieve and display the details of a specific post
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'blog/post_detail.html', {'post': post})
